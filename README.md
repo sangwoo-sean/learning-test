57640889c0a27bd1e242cc9d03c9d7aa93d881c8
sangwoo <tkddn3367@gmail.com>
2022-09-03 15:49:51 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring Boot for Learning Test
---
+++ src/test/java/learning/tester/TesterApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
