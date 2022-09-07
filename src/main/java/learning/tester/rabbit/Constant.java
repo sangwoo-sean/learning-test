dd58b1616db13872559334aec7e8d754fd95ba3b
sangwoo <tkddn3367@gmail.com>
2022-09-07 20:48:51 +0900

    rabbitMQ basic

---
+++ src/main/java/learning/tester/rabbit/Constant.java
@@ -0,0 +1,6 @@
+package learning.tester.rabbit;
+
+public class Constant {
+    public static final String topicExchangeName = "spring-boot-exchange";
+    public static final String queueName = "spring-boot";
+}
