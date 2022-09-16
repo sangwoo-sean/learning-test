3ef5a7a36d72b64c770c79d2fa73efc763503f49
sangwoo <tkddn3367@gmail.com>
2022-09-16 23:12:11 +0900

    reactive with webclient

--- src/main/java/learning/tester/webflux/WebfluxController.java
+++ src/main/java/learning/tester/webflux/WebfluxController.java
@@ -3,22 +3,16 @@ package learning.tester.webflux;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.Disposable;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 @RestController
 @Slf4j
 public class WebfluxController {
-
-    @GetMapping("flux")
-    public String webflux() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return "hello";
-    }
 
     @GetMapping("mono")
     public Mono<String> mono() {
@@ -46,4 +40,34 @@ public class WebfluxController {
         return mono;
     }
 
+    @GetMapping("flux")
+    public Flux<Integer> flux() {
+        Stream<Integer> stream = Stream.iterate(0, i -> i + 1).limit(100);
+
+        return Flux.interval(Duration.ofSeconds(3)).fromStream(stream);
+//        return Flux.just("Hello", "World", "!");
+    }
+
+    @GetMapping("webclient")
+    public Mono<String> webclient() {
+        WebClient webClient = WebClient.create("http://localhost:8080");
+
+        return webClient.get()
+                .uri("/mono")
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("sub")
+    public String sub() {
+        WebClient webClient = WebClient.create("http://localhost:8080");
+
+        webClient.get()
+                .uri("/mono")
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(System.out::println);
+
+        return "requesting...";
+    }
 }
