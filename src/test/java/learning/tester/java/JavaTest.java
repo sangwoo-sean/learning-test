ec5bce9e56ed3c6f7d471a580aec84b89b407666
sangwoo <tkddn3367@gmail.com>
2022-10-10 12:14:22 +0900

    [test] StringUtils 학습테스트

---
+++ src/test/java/learning/tester/java/JavaTest.java
@@ -0,0 +1,44 @@
+package learning.tester.java;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JavaTest {
+
+    @Nested
+    class StringUtilsTest {
+
+        @Test
+        void hasText() {
+            assertFalse(StringUtils.hasText(null));
+            assertFalse(StringUtils.hasText(""));
+            assertFalse(StringUtils.hasText("   "));
+            assertTrue(StringUtils.hasText("  text "));
+        }
+
+        @Test
+        void containsWhitespace() {
+            assertFalse(StringUtils.containsWhitespace(""));
+            assertTrue(StringUtils.containsWhitespace("   "));
+            assertTrue(StringUtils.containsWhitespace("   A "));
+            assertFalse(StringUtils.containsWhitespace("A"));
+            assertFalse(StringUtils.containsWhitespace(null));
+        }
+
+        @Test
+        void trimWhitespaces() {
+            assertEquals("A", StringUtils.trimAllWhitespace("   A   "));
+            assertEquals("A   ", StringUtils.trimLeadingWhitespace("   A   "));
+            assertEquals("   A", StringUtils.trimTrailingWhitespace("   A   "));
+        }
+
+        @Test
+        void trimCharacters() {
+            assertEquals("BBAABB", StringUtils.trimLeadingCharacter("AABBAABB", 'A'));
+            assertEquals("AABBAA", StringUtils.trimTrailingCharacter("AABBAABB", 'B'));
+        }
+    }
+}
