3bce9d2d7a1077701ea119c3eb4a67252162e7fb
sangwoo <tkddn3367@gmail.com>
2022-10-13 21:48:07 +0900

    여러개 테스트 한번에 실행

--- src/main/java/learning/tester/XUnit/TestCaseTest.java
+++ src/main/java/learning/tester/XUnit/TestCaseTest.java
@@ -8,14 +8,16 @@ public class TestCaseTest extends TestCase {
 
     public void testTemplateMethod() {
         WasRun wasRun = new WasRun("testMethod");
-        wasRun.run();
+        TestResult testResult = new TestResult();
+        wasRun.run(testResult);
         Assert.assertEquals("setUp testMethod tearDown", wasRun.log);
     }
 
     public void testResult() {
         WasRun wasRun = new WasRun("testMethod");
-        TestResult testResult = wasRun.run();
-        Assert.assertEquals("1 run, 0 failed", testResult.getSummary());
+        TestResult result = new TestResult();
+        wasRun.run(result);
+        Assert.assertEquals("1 run, 0 failed", result.getSummary());
     }
 
     public void testFailedResultFormatting() {
@@ -27,7 +29,17 @@ public class TestCaseTest extends TestCase {
 
     public void testFailedResult() {
         WasRun wasRun = new WasRun("testBrokenMethod");
-        TestResult result = wasRun.run();
+        TestResult result = new TestResult();
+        wasRun.run(result);
         Assert.assertEquals("1 run, 1 failed", result.getSummary());
+    }
+
+    public void testSuite() {
+        TestSuite suite = new TestSuite();
+        suite.add(new WasRun("testMethod"));
+        suite.add(new WasRun("testBrokenMethod"));
+        TestResult result = new TestResult();
+        suite.run(result);
+        Assert.assertEquals("2 run, 1 failed", result.getSummary());
     }
 }
---
+++ src/main/java/learning/tester/XUnit/TestSuite.java
@@ -0,0 +1,18 @@
+package learning.tester.XUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSuite {
+    List<WasRun> tests = new ArrayList<>();
+
+    public void add(WasRun test) {
+        tests.add(test);
+    }
+
+    public void run(TestResult result) {
+        tests.forEach(t -> {
+            t.run(result);
+        });
+    }
+}
