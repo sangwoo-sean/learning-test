5e31ff7ff486d9842e3e8f8765e6c7908df97ee9
sangwoo <tkddn3367@gmail.com>
2022-10-13 21:09:14 +0900

    테스트 코드 init

---
+++ src/main/java/learning/tester/XUnit/TestCaseTest.java
@@ -0,0 +1,15 @@
+package learning.tester.XUnit;
+
+public class TestCaseTest extends TestCase {
+
+    public TestCaseTest(String name) {
+        super(name);
+    }
+
+    public void testRunning() {
+        WasRun wasRun = new WasRun("testMethod");
+        Assert.assertEquals(false, wasRun.wasRun);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasRun);
+    }
+}
---
+++ src/main/java/learning/tester/XUnit/XUnit.java
@@ -0,0 +1,7 @@
+package learning.tester.XUnit;
+
+public class XUnit {
+    public static void main(String[] args) {
+        new TestCaseTest("testRunning").run();
+    }
+}
