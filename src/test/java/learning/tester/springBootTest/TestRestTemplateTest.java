11d2dd2ee90f3f7a26d124294506bc494f868e67
sangwoo <tkddn3367@gmail.com>
2022-10-23 21:02:53 +0900

    refactor

--- src/test/java/learning/tester/springBootTest/TestRestTemplateTest.java
+++ src/test/java/learning/tester/springBootTest/TestRestTemplateTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SBTest {
+class TestRestTemplateTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
